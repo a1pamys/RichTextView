5ec66c0a216cbc6dd5cdbb896daf154dddd8a2fe
Alpamys <dalpamysn@gmail.com>
2020-08-09 16:20:21 +0600

    fix: small fixes

--- Source/Extensions/NSMutableAttributedStringExtension.swift
+++ Source/Extensions/NSMutableAttributedStringExtension.swift
@@ -16,12 +16,13 @@ extension NSAttributedString {
         let text = NSMutableAttributedString(attributedString: self)
         text.enumerateAttribute(NSAttributedString.Key.attachment, in: NSMakeRange(0, text.length), options: .init(rawValue: 0), using: { (value, range, stop) in
             if let attachement = value as? NSTextAttachment {
-                let image = attachement.image(forBounds: attachement.bounds, textContainer: NSTextContainer(), characterIndex: range.location)!
-                if image.size.width > maxWidth {
-                    let newImage = image.resizeImage(scale: maxWidth/image.size.width)
-                    let newAttribut = NSTextAttachment()
-                    newAttribut.image = newImage
-                    text.addAttribute(NSAttributedString.Key.attachment, value: newAttribut, range: range)
+                if let image = attachement.image(forBounds: attachement.bounds, textContainer: NSTextContainer(), characterIndex: range.location) {
+                    if image.size.width > maxWidth {
+                        let newImage = image.resizeImage(scale: maxWidth/image.size.width)
+                        let newAttribut = NSTextAttachment()
+                        newAttribut.image = newImage
+                        text.addAttribute(NSAttributedString.Key.attachment, value: newAttribut, range: range)
+                    }                    
                 }
             }
         })
